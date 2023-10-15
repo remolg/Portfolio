caa2c990c6a76072d0b50220b5eefb5cf68be8d8
Remolg <sadikahmet25@gmail.com>
2023-10-15 23:43:19 +0300

    Headercss loading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
-import "./Header.css"
+import "./header.css"
 
 export const Header = () => {
     return (
         <header className="header">
             <nav className='nav container'>
-                <a href="index.html" className="nav__logo">Logo</a>
+                <a href="index.html" className="nav__logo">REMOLG</a>
+
                 <div className="nav__menu">
+                    <ul className='nav__list'>
+                        <li className="nav__item">
+                            <a href="#home" className='nav__link active-link'>
+                                <i className='uil uil-estate nav__icon'></i>
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#about" className='nav__link'>
+                                <i className='uil uil-user nav__icon'></i>
+                                About
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#skills" className='nav__link'>
+                                <i className='uil uil-file-alt nav__icon'></i>
+                                Skills
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="portfolio" className='nav__link'>
+                                <i className='uil uil-scenery nav__icon'></i>
+                                Portfolio
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="contact" className='nav__link'>
+                                <i className='uil uil-message nav__icon'></i>
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+
+                    <i className='uil uil-times nav__close'></i>
+
+                    <div className="nav__toggle">
+                        <i className='uil uil-apps'></i>
+                    </div>
                 </div>
             </nav>
         </header>
