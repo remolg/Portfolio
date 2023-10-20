1a1e66900d6f9f0b16b8fbac51551cf266eb648c
Remolg <sadikahmet25@gmail.com>
2023-10-20 05:04:48 +0300

    Contact section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header/Header'
 import { Home } from './components/Home/Home'
 import { About } from './components/about/About'
 import { Skills } from './components/skills/Skills'
+import { Contact } from './components/contact/Contact'
 import './App.css'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Contact />
       </main>
     </>
   )
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import './contact.css'
+import { Hello } from '../Home/Homeicons'
+
+export const Contact = () => {
+    return (
+        <section className="contact section" id="contact">
+            <h2 className='section__title'>Get in touch</h2>
+            <span className='section__subtitle'>Contact Me</span>
+
+            <div className="contact__container container grid">
+                <div className="contact__content">
+                    <h3 className="contact__title">Talk to me</h3>
+                    <div className="contact__info">
+                        <div className="contact__card">
+                            <i className="bx bx-mail-send contact__card-icon"></i>
+
+                            <h3 className="contact__card-title">Email</h3>
+                            <span className="contact__card-data">sadikahmet25@gmail.com</span>
+
+                            <a href="mailto:sadikahmet25@gmail.com" className="contact__button">Write me{' '} <i className='bx bx-right-arrow-alt contact__button-icon'></i></a>
+                        </div>
+                        <div className="contact__card">
+                            <i className="uil uil-discord contact__card-icon"></i>
+
+                            <h3 className="contact__card-title">Discord</h3>
+                            <span className="contact__card-data">remolgcum</span>
+
+                            <a href="https://discord.com/" className="contact__button">Write me{' '} <i className='bx bx-right-arrow-alt contact__button-icon'></i></a>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="contact__content">
+                    <h3 className="contact-title">Write me your project</h3>
+
+                    <form className="contact__form">
+                        <div className="contact__form-div">
+                            <label className="contact__form-tag">Name</label>
+                            <input type="text" name='name' className='contact__form-input' placeholder='Insert your name' />
+                        </div>
+                        <div className="contact__form-div">
+                            <label className="contact__form-tag">Mail</label>
+                            <input type="email" name='email' className='contact__form-input' placeholder='Insert your email' />
+                        </div>
+                        <div className="contact__form-div">
+                            <label className="contact__form-tag">Project</label>\
+                            <textarea name="project" cols="30" rows="10" className='contact__form-input' placeholder='Write your project'></textarea>
+                        </div>
+
+                        <button className="button button--flex">
+                            Send message
+                            <Hello />
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    )
+}
