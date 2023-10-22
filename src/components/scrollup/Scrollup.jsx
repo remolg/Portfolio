38ffca69c8fbf3db1cac9eac04115a9ee033aa47
Remolg <sadikahmet25@gmail.com>
2023-10-22 04:34:25 +0300

    ScrollUp edited

--- src/components/scrollup/Scrollup.jsx
+++ src/components/scrollup/Scrollup.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './scrollup.css'
 
 export const Scrollup = () => {
-    window.addEventListener('scroll', () => {
-        const scrollUp = document.querySelector('.scrollup');
-        if (window.scrollY >= 560) scrollUp.classList.add('show-scroll');
-        else scrollUp.classList.remove('show-scroll');
-    })
+    const [showScroll, setShowScroll] = useState(false)
+
+    const handleScroll = () => {
+        if (window.scrollY >= 560) {
+            setShowScroll(true)
+        } else {
+            setShowScroll(false)
+        }
+    }
+
+    const scrollTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+    }, [])
+
+
     return (
-        <a href="#" className="scrollup">
+        <a href="#" className={`scrollup ${showScroll ? `show-scroll` : ''}`} onClick={scrollTop}>
             <i className="uil uil-arrow-up scrollup__icon"></i>
         </a>
     )
