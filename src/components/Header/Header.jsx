e986f464faf5c6d0a3c7f5e54383e6ec37a462f9
Remolg <sadikahmet25@gmail.com>
2023-11-28 17:06:50 +0300

    Header edited

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,7 +39,7 @@ export const Header = () => {
                         <li className="nav__item">
                             <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'nav__link active-link' : 'nav__link'}>
                                 <i className='uil uil-scenery nav__icon'></i>
-                                Portfolio
+                                Projects
                             </a>
                         </li>
                         <li className="nav__item">
