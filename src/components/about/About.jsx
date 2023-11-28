e9365257c9d15920d3607c0351adfcf43b0a9bff
Remolg <sadikahmet25@gmail.com>
2023-11-28 15:43:53 +0300

    CV name edited

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './about.css'
 import AboutImg from '../../assets/about.jpg'
-import CV from '../../assets/cv.pdf'
+import CV from '../../assets/sadikahmetozdemirCV.pdf'
 import { Files } from './Abouticons'
 import { Info } from './Info'
 
